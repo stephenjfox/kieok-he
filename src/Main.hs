601467a604e6f069e3a9eb2e037089425e2be576
Stephen Fox <stevethecoder34@gmail.com>
2016-11-10 23:33:18 -0700

    Module for user interaction
    For this first iteration, this is the process:
      1. Load the change from memory
      2. Mutate state based on user action
      3. Write back out to disk
    
    It will be slow, but it should work. Simplicity first, then plug
      in the intricacies.

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,6 @@
 module Main where
 
-type DataKey = String
-
-{-
-The standard CRUD can be reduced to "Update" and "Read", or Fetch/Put in our case.
-"Update" is an overwrite, not a compute. Overwriting nothingness with a value,
-  simply begets the value.
-  Likewise, overwriting existence with nothingness is the logical delete. Users
-  will see a delete command, but the backing implementation will remain simple
-"Fetch" is a read. Simple. STUPID simple.
-  There is some degree of encoding that is going to need persistence, but this
-  will be developed in a flexible fashion, such that inserting the change won't
-  break the system.
--}
-data Action = Fetch { key :: DataKey }
-            | Put { key :: DataKey, value :: String }
-            deriving (Show)
+import           User
 
 main :: IO ()
 main = do
