b88bd34c504306050967747236bd63d9d691b178
Stephen Fox <stevethecoder34@gmail.com>
2016-11-10 23:26:55 -0700

    Implement Action records for the reduction-based computition
    There is a popular idea of reducing state from a sequence of
      actions, into the current state. A front-end library, Redux,
      has made this very popular.
      Current plans are to handle the state of the database in a
      similar "transactional" style. Actual source of the idea
      is Functional Programming reduction of the following:
        map, reduce/fold, filter,

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+type DataKey = String
+
+{-
+The standard CRUD can be reduced to "Update" and "Read", or Fetch/Put in our case.
+"Update" is an overwrite, not a compute. Overwriting nothingness with a value,
+  simply begets the value.
+  Likewise, overwriting existence with nothingness is the logical delete. Users
+  will see a delete command, but the backing implementation will remain simple
+"Fetch" is a read. Simple. STUPID simple.
+  There is some degree of encoding that is going to need persistence, but this
+  will be developed in a flexible fashion, such that inserting the change won't
+  break the system.
+-}
+data Action = Fetch { key :: DataKey }
+            | Put { key :: DataKey, value :: String }
+            deriving (Show)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Hello, 기억해!"
+  print $ Fetch "cat"
+  print $ Put "cat" "fiddle"
